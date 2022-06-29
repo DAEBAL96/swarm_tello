337e025b6af3ee3650db552c69a54befa6448cc6
daebardaebar <108359507+daebardaebar@users.noreply.github.com>
2022-06-30 02:37:14 +0900

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -11,7 +11,7 @@ import torch
 
 from models.yolo import Model
 from utils.general import set_logging
-from utils.google_utils import attempt_download
+from utils.downloads import attempt_download
 
 dependencies = ['torch', 'yaml']
 set_logging()
